18f7bf0b37fbf1d4eec1197d5a91e83bad660f4d
M. Selcuk Candan <mehmetselcukcandan@icloud.com>
2022-11-03 10:17:21 +0300

    feat: update lspconfig

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -81,7 +81,7 @@ nvim_lsp.cssls.setup {
 
 nvim_lsp.tsserver.setup {
   on_attach = on_attach,
-  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+  filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact" },
   cmd = { "typescript-language-server", "--stdio" },
   capabilities = capabilities
 }
