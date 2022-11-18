73165b8f3e456f486f886851d27538754d4fe9e0
M. Selcuk Candan <mehmetselcukcandan@icloud.com>
2022-11-18 10:20:17 +0300

    feat: compile packer

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -129,6 +129,11 @@ _G.packer_plugins = {
     path = "/Users/mscandan/.local/share/nvim/site/pack/packer/start/markdown-preview.nvim",
     url = "https://github.com/iamcco/markdown-preview.nvim"
   },
+  ["mason.nvim"] = {
+    loaded = true,
+    path = "/Users/mscandan/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    url = "https://github.com/williamboman/mason.nvim"
+  },
   ["null-ls.nvim"] = {
     loaded = true,
     path = "/Users/mscandan/.local/share/nvim/site/pack/packer/start/null-ls.nvim",
