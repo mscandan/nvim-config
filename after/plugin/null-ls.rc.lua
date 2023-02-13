e942016692fbac3ac9df2d6e077ab146a8e97589
M. Selcuk Candan <mehmetselcukcandan@icloud.com>
2023-02-13 10:30:03 +0300

    refactor: use eslint_d instead of eslint

--- after/plugin/null-ls.rc.lua
+++ after/plugin/null-ls.rc.lua
@@ -6,8 +6,10 @@ local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.clang_format,
+    null_ls.builtins.formatting.eslint_d,
     null_ls.builtins.formatting.rustfmt,
-    null_ls.builtins.diagnostics.eslint.with({
+    null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.diagnostics.eslint_d.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
     null_ls.builtins.diagnostics.zsh
