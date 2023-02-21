73a40850e7d2f24feeb9d66588c4b427f5065425
M. Selcuk Candan <mehmetselcukcandan@icloud.com>
2023-02-21 11:18:53 +0300

    fix: change lualine colorschema

--- after/plugin/lualine.rc.lua
+++ after/plugin/lualine.rc.lua
@@ -4,7 +4,7 @@ if (not status) then return end
 lualine.setup {
   options = {
     icons_enabled = true,
-    theme = 'gruvbox',
+    theme = 'onedark',
     section_separators = { left = '', right = '' },
     component_separators = { left = '', right = '' },
     disabled_filetypes = {}
