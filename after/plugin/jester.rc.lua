de17c96b8115f295b0731b569d94444e03c5b391
M. Selcuk Candan <mehmetselcukcandan@icloud.com>
2023-08-09 10:59:19 +0300

    feat: update jester

--- after/plugin/jester.rc.lua
+++ after/plugin/jester.rc.lua
@@ -3,15 +3,14 @@ local status, jester = pcall(require, "jester")
 if not status then return end
 
 jester.setup({
-  cmd = "./node_modules/.bin/jest $file -t $result", -- run command
-  identifiers = { "test", "it" }, -- used to identify tests
-  prepend = { "describe" }, -- prepend describe blocks
-  expressions = { "call_expression" }, -- tree-sitter object used to scan for tests/describe blocks
-  path_to_jest_run = './node_modules/.bin/jest', -- used to run tests
-  path_to_jest_debug = './node_modules/.bin/jest', -- used for debugging
-  terminal_cmd = ":vsplit | terminal", -- used to spawn a terminal for running tests, for debugging refer to nvim-dap's config
+  cmd = "./node_modules/.bin/jest -t '$result' -- $file", -- run command
+  identifiers = { "test", "it" },                         -- used to identify tests
+  prepend = { "describe" },                               -- prepend describe blocks
+  expressions = { "call_expression" },                    -- tree-sitter object used to scan for tests/describe blocks
+  path_to_jest_run = 'jest',                              -- used to run tests
+  path_to_jest_debug = './node_modules/.bin/jest',        -- used for debugging
+  terminal_cmd = ":vsplit | terminal",                    -- used to spawn a terminal for running tests, for debugging refer to nvim-dap's config
 })
-
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<Space>tc', '<Cmd>lua require("jester").run()<CR>', opts)
