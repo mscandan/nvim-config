c7ba72f1815e0c39acf36da2ff0a237a904636c7
M. Selcuk Candan <mehmetselcukcandan@icloud.com>
2023-08-13 21:43:36 +0300

    feat: add 'fatih/vim-go'

--- lua/mscandan/plugins.lua
+++ lua/mscandan/plugins.lua
@@ -48,4 +48,7 @@ return require('packer').startup(function(use)
       { 'L3MON4D3/LuaSnip' },     -- Required
     }
   }
+
+  --golang
+  use 'fatih/vim-go'
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -173,6 +173,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/mscandan/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
+  },
+  ["vim-go"] = {
+    loaded = true,
+    path = "/Users/mscandan/.local/share/nvim/site/pack/packer/start/vim-go",
+    url = "https://github.com/fatih/vim-go"
   }
 }
 
