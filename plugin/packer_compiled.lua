deffea46a326f8bfab8e0a60cd55177055ce5960
M. Selcuk Candan <mehmetselcukcandan@icloud.com>
2024-05-20 11:13:51 +0300

    feat: update plugins

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/mscandan/.cache/nvim/packer_hererocks/2.1.1703358377/share/lua/5.1/?.lua;/Users/mscandan/.cache/nvim/packer_hererocks/2.1.1703358377/share/lua/5.1/?/init.lua;/Users/mscandan/.cache/nvim/packer_hererocks/2.1.1703358377/lib/luarocks/rocks-5.1/?.lua;/Users/mscandan/.cache/nvim/packer_hererocks/2.1.1703358377/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/mscandan/.cache/nvim/packer_hererocks/2.1.1703358377/lib/lua/5.1/?.so"
+local package_path_str = "/Users/mscandan/.cache/nvim/packer_hererocks/2.1.1713773202/share/lua/5.1/?.lua;/Users/mscandan/.cache/nvim/packer_hererocks/2.1.1713773202/share/lua/5.1/?/init.lua;/Users/mscandan/.cache/nvim/packer_hererocks/2.1.1713773202/lib/luarocks/rocks-5.1/?.lua;/Users/mscandan/.cache/nvim/packer_hererocks/2.1.1713773202/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/mscandan/.cache/nvim/packer_hererocks/2.1.1713773202/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -198,6 +198,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/mscandan/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  ["ts-error-translator.nvim"] = {
+    loaded = true,
+    path = "/Users/mscandan/.local/share/nvim/site/pack/packer/start/ts-error-translator.nvim",
+    url = "https://github.com/dmmulroy/ts-error-translator.nvim"
   },
   ["vim-fugitive"] = {
     loaded = true,
